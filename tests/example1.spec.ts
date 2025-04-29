000f2b98891c43c3ceb1ae442411c5292880f749
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-04-29 16:46:02 +0530

    testing

--- tests/example1.spec.ts
+++ tests/example1.spec.ts
@@ -3,7 +3,7 @@ import { test } from "@playwright/test";
 test('has title', async ({ page }) => {
 
 await page.goto('https://www.youtube.com/watch?v=r6m1TilR0eU');
-
+await page.waitForTimeout(5000)
 const skipbutton=await page.locator("//*[text()='Skip']").isVisible()
 if(skipbutton){
 await page.locator("//button[@aria-label='Pause (k)']").click();
