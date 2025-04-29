b01f8453698eb6831a1e855661d8f08e6a36fa8a
senthilkumarq <43035170+senthilkumarq@users.noreply.github.com>
2025-04-29 09:00:57 +0530

    testingg

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless:true
+    headless:true,
+    video:'on',
+    screenshot:'on'
   },
 
   /* Configure projects for major browsers */
